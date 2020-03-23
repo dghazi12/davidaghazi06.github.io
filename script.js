14dbaa671d6981f362ba1eb6fed2211ce871f0a5
David Ghazi <davidaghazi@gmail.com>
2020-03-23 12:33:41 -0400

    now can make proper api calls to open weather map

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-$("#button-addon1").on("click", function(){
+//This function, once ready, will make a request to the openweathermap API and retrieve all of the data
+$(document).ready(function () {
+    $("#button-addon1").click(function () {
 
-    const userInput = $('.form-control').val();
+        const queryURL = 'http://api.openweathermap.org/data/2.5/weather'
+        const city = $('.form-control').val();
+        const key = '52af5110a99ad9f2762ddfe25f5f2b69'
+
+        $.ajax({
+            url: queryURL,
+            dataType: 'json',
+            type: 'GET',
+            data: {q: city, appid: key, units: 'metric'},
+
+            success: function (data) {
+
+                console.log(data)
+
+            }
+
+        });
+
+        saveCity()
+
+    });
+
+});
+
+// This function appends all of the users search to the webpage
+function saveCity() {
+
+    const city = $('.form-control').val();
 
     const cityList = $('.list-group-item').append('<ul></ul>');
-    cityList.append(userInput)
-    
+    cityList.append(city)
+
     $('.form-control').val('')
-})
+}
+
+function 
